eb23dc8cdb51c65861df538a22ed20caaa0e01ba
ngohoang29 <ngohoang29@gmail.com>
2021-05-05 20:14:08 +0700

    commit file

--- DemoWithAjax/Controllers/StudentController.cs
+++ DemoWithAjax/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using DemoWithAjax.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,9 +25,27 @@ namespace DemoWithAjax.Controllers
             return Json(new {Data=results,TotalItems=results.Count},JsonRequestBehavior.AllowGet);
         }
 
+        public ActionResult GetById(int id)
+        {
+            var item = _context.Students.Find(id);
+            return Json(new { data = item },JsonRequestBehavior.AllowGet);
+        }
+
         [HttpPost]
         public ActionResult Create(Student model)
         {
+            if (model.Id > 0)
+            {
+                _context.Students.Attach(model);
+                model.CreatedDate = DateTime.Now;
+                //var item = _context.Students.Find(model.Id);
+                //item.Name = model.Name;
+                //item.Age = model.Age;
+                //item.Class = model.Class;
+                _context.Entry(model).State = EntityState.Modified;
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
             model.CreatedDate = DateTime.Now;
             _context.Students.Add(model);
             try
@@ -40,6 +59,28 @@ namespace DemoWithAjax.Controllers
                 return Json(new { success = false });
             }
            
+        }
+
+        [HttpPost]
+        public ActionResult Update(Student request)
+        {
+            var student = _context.Students.Find(request.Id);
+            _context.Entry(student).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public JsonResult DeleteRecord(int id)
+        {
+            var student = _context.Students.Find(id);
+            _context.Students.Remove(student);
+            var rs=_context.SaveChanges();
+            if (rs > 0)
+            {
+                return Json(new { Success = true });
+            }
+            return Json(new { Success = false });
         }
     }
 }
